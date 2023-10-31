8d670a2e025917de730516bc3ebea6b54ac9f599
yann64 <yann64@users.noreply.github.com>
2023-10-31 14:28:38 +0100

    add userguide

--- createmirror.sh
+++ createmirror.sh
@@ -1,38 +1,37 @@
 # Change the following values to configure your mirror :
+
 # Path to the Hugo binary
-HUGO_PATH="/c/hugo/hugo.exe"
+HUGO_PATH="/c/Local/hugo/hugo.exe"
 # The mirror website title
 MIRROR_TITLE="Haiku Project (Mirror Site)"
 # The mirror base URL (must end with a slash '/')
 MIRROR_BASEURL="http://localhost/haiku/"
 # Where the files will be copied
-MIRROR_LOCALPATH="/c/xampp/htdocs/haiku" 
+MIRROR_LOCALPATH="/c/Local/PortableApps/PortableApps/XAMPP/App/xampp/htdocs/haiku" 
 
-# First we clone the website latest version
-echo "==> Clone from official repository :"
+# Main site
+echo "==> Clone website from the official repository :"
 git clone https://github.com/haiku/website.git
-
-# We then enter the website repository
 cd website
-
-# We change the site title
 sed -i "s/title = \"Haiku Project\"/title = \"$MIRROR_TITLE\"/" ./config.toml
-
-# We change the base URL
 sed -i "s@baseURL = \"https://www.haiku-os.org/\"@baseURL = \"$MIRROR_BASEURL\"@" ./config.toml
-
-# We generate the site
 echo "==> Site generation using Hugo :"
 $HUGO_PATH
-
-# Copy the website to the target folder
 echo "==> Copy files to destination forlder: "$MIRROR_LOCALPATH
 cd  public
 cp -r * $MIRROR_LOCALPATH
-
-# Clean
 echo "==> Cleaning"
 cd ../..
 rm -rf ./website
+
+# Clone the userguide latest version
+echo "==> Clone userguide from the official repository :"
+git clone "https://review.haiku-os.org/userguide"
+cd userguide
+echo "==> Copy files to destination forlder: $MIRROR_LOCALPATH/docs"
+cp -r * "$MIRROR_LOCALPATH/docs"
+echo "==> Cleaning"
+cd ..
+rm -rf ./userguide
 
 echo "Done !"
